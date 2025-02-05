e969d68f24fb412818ebc2cf0748262f31086ed6
Whale <revletos@gmail.com>
2025-02-05 10:58:17 -0500

    Added script

---
+++ Assets/Scripts/Coding Gym.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CodingGym : MonoBehaviour
+{
+    public Button btn;
+    public AudioSource audiosource;
+    public AudioClip clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
