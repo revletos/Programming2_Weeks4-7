b9b1db9f36ca05dbd7b67e09b90c5b576517538a
belin <belin@OA-S141-22>
2025-02-12 10:25:25 -0500

    Committing and Pushing to not lose work - Added Tank and all relevant sprites - Alpha versions of movement and aim scripts. Both need adjustments. Adjustments for both scripts are commented at the beginning of void update

---
+++ Assets/Scripts/TankMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    public float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //REPLACE THIS CODE LATER WITH ACTUAL CODE
+        //THIS IS JUST COPY PASTED TO MAKE SURE ALL ELEMENTS MOVE TOGETHER PROPERLY
+
+        //things to note: Tank only switches directions when CENTER of tank hits edge of screen. 
+        //See how that could be taken into account with code
+        //Speed should be at least 1 from the get go
+        Vector2 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        Vector2 squareInScreenSpace = Camera.main.WorldToScreenPoint(pos);
+
+        if (squareInScreenSpace.x < 0 || squareInScreenSpace.x > Screen.width)
+        {
+            speed = speed * -1;
+        }
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/TurretAim.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretAim : MonoBehaviour
+{
+    public float speed = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Adjust to limit aim (See Pseudocode for more information)
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouse.z = 0;
+        Vector2 direction = mouse - transform.position;
+
+        transform.up = direction;
+    }
+}
